eec61d2f8cad6f82abb82712b0a169e19e316046
Marco Solina <marcosolina@gmail.com>
2021-05-20 21:53:20 +0200

    Update startAll.sh

--- Scripts/startAll.sh
+++ Scripts/startAll.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+EVENT_FILE=$ENV_CSGO_INSTALL_FOLDER/csgo/addons/sourcemod/event.txt
+echo "NO" > $EVENT_FILE
+
 $SCRIPT_DIR/startHelper.sh start
 nohup $SCRIPT_DIR/startEventsMonitor.sh >> /tmp/eventMonitor.log 2&1&
 $SCRIPT_DIR/startIxigoServer.sh
