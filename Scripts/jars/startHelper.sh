d5582ec98e1234ac7af9b0b40b92f3c8b21896db
Marco Solina <Marco.Solina@ixico.com>
2021-07-13 22:46:29 +0200

    test

--- Scripts/jars/startHelper.sh
+++ Scripts/jars/startHelper.sh
@@ -10,21 +10,25 @@ APP_JAR_2=IxigoDiscordBot.jar
 APP_NOHUP_2=${APP_FOLDER}IxigoDiscordBot.log
 APP_PID_FILE_2=${APP_FOLDER}IxigoDiscordBot.pid
 
+export HOST_IP=$(hostname -I | awk '{print $1}')
+
 case "$1" in
 start)
    cd $APP_FOLDER
    nohup java -jar $APP_JAR_1 >> $APP_NOHUP_1 2>&1&
    echo $!>$APP_PID_FILE_1
    chmod 775 $APP_PID_FILE_1
-   nohup java -jar $APP_JAR_2 >> $APP_NOHUP_2 2>&1&
-   echo $!>$APP_PID_FILE_2
-   chmod 775 $APP_PID_FILE_2
+   
+   #nohup java -jar $APP_JAR_2 >> $APP_NOHUP_2 2>&1&
+   #echo $!>$APP_PID_FILE_2
+   #chmod 775 $APP_PID_FILE_2
    ;;
 stop)
    kill `cat $APP_PID_FILE_1`
    rm $APP_PID_FILE_1
-   kill `cat $APP_PID_FILE_2`
-   rm $APP_PID_FILE_2
+   
+   #kill `cat $APP_PID_FILE_2`
+   #rm $APP_PID_FILE_2
    ;;
 restart)
    $0 stop
@@ -36,12 +40,12 @@ status)
    else
       echo RoundParser is NOT running
    fi
-   if [ -e $APP_PID_FILE_2 ]; then
-      echo RoundParser is running, pid=cat $APP_PID_FILE_2
-   else
-      echo RoundParser is NOT running
-      exit 1
-   fi
+   #if [ -e $APP_PID_FILE_2 ]; then
+   #   echo RoundParser is running, pid=cat $APP_PID_FILE_2
+   #else
+   #   echo RoundParser is NOT running
+   #   exit 1
+   #fi
    ;;
 *)
    echo "Usage: $0 {start|stop|status|restart}"
