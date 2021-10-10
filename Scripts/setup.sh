da4a3deac4d842da9b3989eb726f2cd6fcb83d59
Marco <Marco@Marco-Pc>
2021-10-10 19:10:48 +0200

    updates

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -7,10 +7,6 @@ fi
 
 apt update
 
-# Clone the repo in the folder specified by the user
-echo ""
-echo ""
-#read -p "Where do you want to install the server? (/full/path/): " INSTALL_PATH
 INSTALL_PATH="/home/ixigo"
 
 # Creating Ixigo Group and User
@@ -18,26 +14,38 @@ IXIGO_GROUP=ixigo
 IXIGO_USER_NAME=ixigo
 IXIGO_USR_PASS=0123456789
 
-addgroup $IXIGO_GROUP
-useradd -s /bin/bash -m -g $IXIGO_GROUP $IXIGO_USER_NAME
-passwd -d $IXIGO_USER_NAME
+SCRIPTS_FOLDER=$INSTALL_PATH/ixi_go/Scripts
+JAR_FOLDER=$INSTALL_PATH/ixi_go/Scripts/jars
+CFG_FOLDER=$INSTALL_PATH/ixi_go/CsgoServer/csgo/cfg
 
-# Setting default user password and
-# forcing passowrd reset at first login
-echo "$IXIGO_USER_NAME:$IXIGO_USR_PASS" | chpasswd
-passwd --expire $IXIGO_USER_NAME
+read -p "Is this a clean install? (y/n): " cleanInstall
 
-echo "$IXIGO_USER_NAME ALL = NOPASSWD: /sbin/shutdown"    | sudo tee -a /etc/sudoers
-
-read -p "Do you want to install the DynDns client? (y/n): " INSTALL_DYNDNS
-if [ $INSTALL_DYNDNS = 'y' ]
+if [ $cleanInstall = 'y' ]
 then
-  apt install -y ddclient
+	addgroup $IXIGO_GROUP
+	useradd -s /bin/bash -m -g $IXIGO_GROUP $IXIGO_USER_NAME
+	passwd -d $IXIGO_USER_NAME
+	
+	# Setting default user password and
+	# forcing passowrd reset at first login
+	echo "$IXIGO_USER_NAME:$IXIGO_USR_PASS" | chpasswd
+	passwd --expire $IXIGO_USER_NAME
+  
+	echo "$IXIGO_USER_NAME ALL = NOPASSWD: /sbin/shutdown"    | sudo tee -a /etc/sudoers
+	
+	read -p "Do you want to install the DynDns client? (y/n): " INSTALL_DYNDNS
+	if [ $INSTALL_DYNDNS = 'y' ]
+	then
+	  apt install -y ddclient
+	fi
+	
+	apt install -y curl git default-jre maven screen
+else
+	cp $CFG_FOLDER/server.cfg /tmp/
+	rm -rf $INSTALL_PATH/ixi_go
 fi
 
-
-apt install -y curl git default-jre maven screen
-
+# Prepare the Jars
 cd /tmp
 git clone https://github.com/marcosolina/csgo_util.git
 git clone https://github.com/marcosolina/javautils.git
@@ -49,11 +57,6 @@ mvn clean package -f ./csgo_util/IxigoDiscordBot/pom.xml -P h2
 
 cd $INSTALL_PATH
 git clone https://github.com/marcosolina/ixi_go.git
-
-
-SCRIPTS_FOLDER=$INSTALL_PATH/ixi_go/Scripts
-JAR_FOLDER=$INSTALL_PATH/ixi_go/Scripts/jars
-CFG_FOLDER=$INSTALL_PATH/ixi_go/CsgoServer/csgo/cfg
 
 sed -i -e 's/\r$//' $JAR_FOLDER/*
 chmod +x $JAR_FOLDER/*
@@ -68,17 +71,24 @@ mv /tmp/csgo_util/IxigoServerHelper/target/IxigoServerHelper*.jar $JAR_FOLDER/Ix
 
 rm -rf /root/.m2/*
 
-# Set the IxiGo Game server passowrd
-echo ""
-echo ""
-read -p "Choose your RCON password: " RCON_PASSWORD
-read -p "Choose your SERVER  password: " SERVER_PASSWORD
 
-sed -i -e "s/RCON_PASSWORD/$RCON_PASSWORD/g" $CFG_FOLDER/server.cfg
-sed -i -e "s/SERVER_PASSWORD/$SERVER_PASSWORD/g" $CFG_FOLDER/server.cfg
+if [ $cleanInstall = 'y' ]
+then
+	# Set the IxiGo Game server passowrd
+	echo ""
+	echo ""
+	read -p "Choose your RCON password: " RCON_PASSWORD
+	read -p "Choose your SERVER  password: " SERVER_PASSWORD
+	
+	sed -i -e "s/RCON_PASSWORD/$RCON_PASSWORD/g" $CFG_FOLDER/server.cfg
+	sed -i -e "s/SERVER_PASSWORD/$SERVER_PASSWORD/g" $CFG_FOLDER/server.cfg
+	
+	echo "Installing Steam CMD"
+	$SCRIPTS_FOLDER/installSteam.sh
+else
+	mv /tmp/server.cfg $CFG_FOLDER
+fi
 
-echo "Installing Steam CMD"
-$SCRIPTS_FOLDER/installSteam.sh
 
 echo "Login with username: $IXIGO_USER_NAME and password: $IXIGO_USR_PASS"
 echo ""
